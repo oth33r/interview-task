91293b352cf1d394b7a1666bf27f69c0322cd1f9
oth33r <ilya_zubkov_1996@mail.ru>
2024-10-13 15:55:20 +0300

    updated routes

--- interview-task/src/main.tsx
+++ interview-task/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import AuthProvider from "./components/AuthProvider.tsx";
 import { Provider } from "react-redux";
@@ -10,11 +10,11 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </StrictMode>
 );
